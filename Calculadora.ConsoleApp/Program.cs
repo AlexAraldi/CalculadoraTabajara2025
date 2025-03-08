3a28f679c78afee48c0f7e1a3102fab14a6a5ab6
alexanderaraldi <alexanderaraldi@gmail.com>
2025-03-08 20:17:07 -0300

    remodelamento duvida no if e else

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace Calculadora.ConsoleApp
+﻿using System.ComponentModel.Design;
+
+namespace Calculadora.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
-            Console.WriteLine("_________________________");
+        {            
+           
             Console.WriteLine("Calculadora Tabajara 2025");
             Console.WriteLine("_________________________");
 
@@ -15,6 +16,8 @@
             Console.WriteLine("Digite 4 para dividir: ");
             Console.WriteLine("Digite S para sair: ");
 
+            //-----------------------------------------------------------------------
+
             Console.Write("Escolha uma opção: ");
 
             string opcao = Console.ReadLine().ToUpper();
@@ -22,9 +25,9 @@
 
             if (opcao == "S" || opcao == "s") {
                 return;
-
             }
-            
+            //-----------------------------------------------------------------------
+          
             Console.Write("Digite o primeiro número: ");
           
             string primeironumerostring = Console.ReadLine();
@@ -35,29 +38,28 @@
             string segundonumerostring = Console.ReadLine();
             int segundonumero = Convert.ToInt32(segundonumerostring);
 
+            //-----------------------------------------------------------------------
+
             int resultado = 0;
             if (opcao == "1") ;
             {
-                 resultado = (primeironumero + segundonumero);
-                
-            }                        
-         
-            if (opcao == "2") ;
+                 resultado = (primeironumero + segundonumero);                
+            }                       
+            if (opcao == "2");
             {
                  resultado = (primeironumero - segundonumero);
-
-            }
-            
+            }            
             if (opcao == "3") ;
             {
                  resultado = (primeironumero * segundonumero);
-
             }         
-            if (opcao == "4") ;
+            if(opcao == "4") ;
             {
                  resultado = (primeironumero / segundonumero);
             }
 
+            //-----------------------------------------------------------------------
+            
             Console.WriteLine("Resultado: " + resultado);
 
 
