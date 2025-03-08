da54ed55c99b886cf493c52aabd5f0784d21b29f
alexanderaraldi <alexanderaraldi@gmail.com>
2025-03-08 16:47:08 -0300

    Adicionado as funcionalidades, duvida no if else nao aparecendo, adicionado as funcoes * /  com bug

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -5,22 +5,65 @@
         static void Main(string[] args)
         {
             
-            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("_________________________");
             Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("_________________________");
 
+            Console.WriteLine("Digite 1 para somar: ");
+            Console.WriteLine("Digite 2 para subtrair: ");
+            Console.WriteLine("Digite 3 para multiplicar: ");
+            Console.WriteLine("Digite 4 para dividir: ");
+            Console.WriteLine("Digite S para sair: ");
+
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine().ToUpper();
+          
+
+            if (opcao == "S" || opcao == "s") {
+                return;
+
+            }
+            
             Console.Write("Digite o primeiro número: ");
-            double numero1 = Convert.ToDouble(Console.ReadLine());
-
+          
+            string primeironumerostring = Console.ReadLine();
+            int  primeironumero = Convert.ToInt32(primeironumerostring);
+            
             Console.Write("Digite o segundo número: ");
-            double numero2 = Convert.ToDouble(Console.ReadLine());
 
-            double resultado = numero1 + numero2;
+            string segundonumerostring = Console.ReadLine();
+            int segundonumero = Convert.ToInt32(segundonumerostring);
 
-            Console.WriteLine();
-            Console.WriteLine($"Resultado da soma: {resultado}");
+            int resultado = 0;
+            if (opcao == "1") ;
+            {
+                 resultado = (primeironumero + segundonumero);
+                
+            }                        
+         
+            if (opcao == "2") ;
+            {
+                 resultado = (primeironumero - segundonumero);
+
+            }
+            
+            if (opcao == "3") ;
+            {
+                 resultado = (primeironumero * segundonumero);
+
+            }         
+            if (opcao == "4") ;
+            {
+                 resultado = (primeironumero / segundonumero);
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+
 
             Console.ReadLine();
+
+
 
 
 
