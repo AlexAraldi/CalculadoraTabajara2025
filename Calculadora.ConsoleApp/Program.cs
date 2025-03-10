40ac927f78dd2ee8a61feab10922357e06e5e9b8
alexanderaraldi <alexanderaraldi@gmail.com>
2025-03-10 18:04:15 -0300

    Atualização - tabuada, definição do "for".

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -1,69 +1,128 @@
 ﻿using System.ComponentModel.Design;
+using System.Linq.Expressions;
 
 namespace Calculadora.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
-        {            
-           
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("_________________________");
+        {
 
-            Console.WriteLine("Digite 1 para somar: ");
-            Console.WriteLine("Digite 2 para subtrair: ");
-            Console.WriteLine("Digite 3 para multiplicar: ");
-            Console.WriteLine("Digite 4 para dividir: ");
-            Console.WriteLine("Digite S para sair: ");
 
-            //-----------------------------------------------------------------------
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("_________________________");
 
-            Console.Write("Escolha uma opção: ");
+                Console.WriteLine("Digite 1 para somar: ");
+                Console.WriteLine("Digite 2 para subtrair: ");
+                Console.WriteLine("Digite 3 para multiplicar: ");
+                Console.WriteLine("Digite 4 para dividir: ");
+                Console.WriteLine("Digite 5 para tabuada: ");
+                Console.WriteLine("Digite S para sair: ");
 
-            string opcao = Console.ReadLine().ToUpper();
-          
+                //-----------------------------------------------------------------------
 
-            if (opcao == "S" || opcao == "s") {
-                return;
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+
+                if (opcao == "S")
+                {
+                    break;
+                }
+                //-----------------------------------------------------------------------
+
+                else if (opcao == "5")
+                {
+
+                    Console.WriteLine("--------------------------------");
+                    Console.WriteLine("Tabuada");
+                    Console.WriteLine("--------------------------------");
+
+                    Console.Write("Digite um número: ");
+                    int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+                    for (int contador = 1; contador <= 10; contador++)
+                    {
+                        
+                        int resultadoTabuada = numeroTabuada * contador;
+                        Console.WriteLine($"{numeroTabuada} x {contador} = {resultadoTabuada}");
+
+                        
+                    }
+                     Console.WriteLine("Fim da operação");
+                     Console.ReadLine( );
+
+                    continue;
+                }
+
+                    //-----------------------------------------------------------------------
+
+                    Console.Write("Digite o primeiro número: ");
+
+                string primeiroNumeroString = Console.ReadLine();
+                decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
+
+                Console.Write("Digite o segundo número: ");
+
+                string segundoNumeroString = Console.ReadLine();
+                decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
+
+                //-----------------------------------------------------------------------
+
+                decimal resultado = 0;
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+                else if (opcao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+                else if (opcao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                }
+                else if (opcao == "4")
+                {
+
+                    while(segundoNumero == 0)
+                    {
+
+                        Console.Write("Não é possível dividir por zero, digite o número novamente: ");
+                        segundoNumero = Convert.ToDecimal (Console.ReadLine());
+
+                    }
+                    resultado = primeiroNumero / segundoNumero;
+
+
+                }
+
+                //-----------------------------------------------------------------------
+
+                Console.WriteLine("Resultado: " + resultado);
+
+                Console.Write("Deseja realizar nova operação? S/N ");
+                string novaoperacao = Console.ReadLine().ToUpper();
+
+
+                if (novaoperacao == "N")
+                {                   
+                    break;
+                }
+
             }
-            //-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
           
-            Console.Write("Digite o primeiro número: ");
-          
-            string primeironumerostring = Console.ReadLine();
-            int  primeironumero = Convert.ToInt32(primeironumerostring);
-            
-            Console.Write("Digite o segundo número: ");
-
-            string segundonumerostring = Console.ReadLine();
-            int segundonumero = Convert.ToInt32(segundonumerostring);
-
-            //-----------------------------------------------------------------------
-
-            int resultado = 0;
-            if (opcao == "1") 
-            {
-                resultado = (primeironumero + segundonumero);                
-            }                       
-            else if (opcao == "2")
-            {
-                resultado = (primeironumero - segundonumero);
-            }
-            else if (opcao == "3")
-            {
-                resultado = (primeironumero * segundonumero);
-             }
-            else if (opcao == "4")
-            {
-                resultado = (primeironumero / segundonumero);
-             }
-
-            //-----------------------------------------------------------------------
-            
-            Console.WriteLine("Resultado: " + resultado);
-
-
-            Console.ReadLine();
 
 
 
